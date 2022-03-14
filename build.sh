8aaf466172a14f23886583a45c64849fee5e2b8a
cakecnc <47858920+cakecnc@users.noreply.github.com>
2022-03-15 01:26:01 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ cargo build --target aarch64-linux-android --release
 cargo build --target armv7-linux-androideabi --release
 cargo build --target i686-linux-android --release
 cargo build --target x86_64-linux-android --release
+rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
+cargo install cargo-lipo
+cargo install cbindgen
+cargo lipo --release
+cbindgen --config cbindgen.toml --crate violet --output my_header.h
